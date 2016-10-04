fc3fb8b466928723e2da7f832fda856fa31f282b
Sydpy <blackstar(ht1>
2016-10-04 16:43:20 +0200

    starting tests

---
+++ src/tests/TarotTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import fr.iut.etu.Card;
+import org.junit.Test;
+
+/**
+ * Created by sdupouy002 on 04/10/16.
+ */
+public class TarotTests {
+
+    @Test
+    public void isCardCreated(){
+
+
+    }
+
+}
