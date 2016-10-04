3699010692eda93f6b587b2c8368fee418cbbbb1
Sydpy <blackstar(ht1>
2016-10-04 16:37:54 +0200

    starting tests

--- test/TarotTests.java
+++ test/TarotTests.java
@@ -2,8 +2,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TarotTests {
-    @Test
-    public void testAdd() {
 
+    @Test
+    public void isCardCreated(){
+        Card card = new Card();
+
+        assertNotEquals("Card not created !", null, card);
     }
+
 }
