2231ed9175bb3d4aa8143c8d2f7451019b4a7b04
Clement Fleury <clement.fleury@etu.u-bordeaux.fr>
2016-11-06 18:09:26 +0100

    Add Material to DeckView

--- src/fr/iut/etu/view/DeckView.java
+++ src/fr/iut/etu/view/DeckView.java
@@ -3,6 +3,9 @@ package fr.iut.etu.view;
 import fr.iut.etu.model.Deck;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 
 import java.util.Observable;
@@ -16,12 +19,19 @@ public class DeckView extends Box implements Observer {
     private Deck deck;
 
     public DeckView(Deck deck) {
-        super(100,200,deck.size());
+        super(100, 200, deck.size());
 
         this.deck = deck;
         deck.addObserver(this);
 
+        //Create the Material
+        PhongMaterial mat = new PhongMaterial();
         Image image = new Image("file:./res/deck.jpg");
+        mat.setDiffuseMap(image);
+        mat.setSpecularColor(Color.BLACK);
+
+        //Apply Material to the Box
+        setMaterial(mat);
 
         Tooltip.install(this, new Tooltip(this.deck.size() + " cards !"));
     }
